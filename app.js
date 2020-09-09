039fc6f83e306e07d2fccbdc015b77fd47db0f8c
David Musko <davmusko@gmail.com>
2020-09-09 15:47:05 -0400

    help, dues, payout, commands added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, token, venmo, payout, dues } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
@@ -8,8 +8,23 @@ client.once('ready', () => {
 
 client.on('message', (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-  if (message.content === `${prefix}ping`) {
-    message.channel.send(message.channel.name);
+
+  if (message.content === `${prefix}commands` || message.content === `${prefix}help`) {
+    message.channel.send(
+      `${prefix}help - List Commands\n${prefix}commands - List Commands\n${prefix}payout - Payout breakdown\n${prefix}dues - Info on due amount and how to pay them\n`
+    );
+  }
+
+  if (message.content === `${prefix}payout`) {
+    message.channel.send(
+      `1st - $${payout[1]}\n2nd - $${payout[2]}\nMost points - $${payout[3]}`
+    );
+  }
+
+  if (message.content === `${prefix}dues`) {
+    message.channel.send(
+      `Dues are ${dues}. Payments can be made to David through Venmo\n${venmo}`
+    );
   }
 });
 
